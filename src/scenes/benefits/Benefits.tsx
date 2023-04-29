b7dbe09f027be6160cacced9bd5308f3ed9fe2d6
Adam <adam@MacBook-Air-Adam.local>
2023-04-30 00:04:34 +0300

    added ourClasses

--- src/scenes/benefits/Benefits.tsx
+++ src/scenes/benefits/Benefits.tsx
@@ -3,9 +3,9 @@ import { motion } from "framer-motion";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const childVariant = {
-   hidden: {opacity: 0, scale: 0.9},
-   visible: {opacity: 1, scale: 1}
-}
+  hidden: { opacity: 0, scale: 0.9 },
+  visible: { opacity: 1, scale: 1 },
+};
 
 type Props = {
   icon: JSX.Element;
